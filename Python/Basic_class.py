10a69e66d9add60a8f72534ecd4bfc1617e33a21
mhkr007 <50578048+mhkr007@users.noreply.github.com>
2019-07-01 12:09:19 +0530

    Create Basic_class.py
    It shows how to write basic oops program in python.

---
+++ Python/Basic_class.py
@@ -0,0 +1,9 @@
+#python 3.5.2
+class person1:
+   def show_chey(self,huhu): #1st parameter(self) list refers to object(per)
+        print("chusuko",huhu)
+per=person1()
+per.show_chey("votary")
+
+
+
